2452491543bcada3955511ab8b318606121215d2
Kuldeep Mali <kuldeepmalikm41@gmail.com>
2021-07-15 13:04:10 +0530

    login UI change

--- src/containers/auth/AuthNavigator.js
+++ src/containers/auth/AuthNavigator.js
@@ -2,14 +2,18 @@ import {createStackNavigator} from '@react-navigation/stack';
 import React from 'react';
 import {
   StatusBar,
+  StyleSheet,
+  TouchableOpacity,
+  Image
 } from 'react-native';
+import images from '../../res/images';
 import LoginScreen from './LoginScreen';
 import SignupScreen from './SignupScreen';
 import ForgotPasswordScreen from './ForgotPasswordScreen';
 
 StatusBar.setBarStyle('light-content');
 
-export default function AuthNavigator() {
+export default function AuthNavigator({navigation}) {
   const androidStatusBar = 0;
   const iosStatusBar = StatusBar.currentHeight + 50;
 
@@ -20,11 +24,21 @@ export default function AuthNavigator() {
         name="Login"
         component={LoginScreen}
         options={{
-          headerStatusBarHeight: Platform.OS === 'android' ? androidStatusBar : iosStatusBar,
-          headerStyle: {backgroundColor: '#000'},
-          headerTintColor: '#fff',
-          headerTransparent: true,
-          title: '',
+          title:'',
+          headerTransparent: false,
+          headerStyle: {
+              backgroundColor: '#5365A2'
+            },
+          headerLeft: () => (
+            <TouchableOpacity style={styles.authBackButton} onPress={()=> navigation.goBack()}>
+                <Image onPress={()=> navigation.goBack()} source={images.backButton}  style={{width:12, height:12}} />
+            </TouchableOpacity>
+          ), 
+          headerRight: () => (
+            <TouchableOpacity style={{right:25}}>
+                <Image onPress={()=> navigation.goBack()} source={images.appIcon}  style={{width:30, height:30}} />
+            </TouchableOpacity>
+          ), 
         }}
       />
       <Stack.Screen
@@ -50,3 +64,14 @@ export default function AuthNavigator() {
     </Stack.Navigator>
   );
 }
+
+styles = StyleSheet.create({
+  authBackButton:{
+    backgroundColor:'rgba(1,41,50, 0.5)',
+    padding:6,
+    borderRadius:15,
+    height:25,
+    width:25,
+    marginStart:10
+  },
+})
